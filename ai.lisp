739c34e290f34fceea24a426550a4b9312feb2fa
Blake McBride <blake@arahant.com>
2015-07-05 22:54:52 -0500

    Added make-structure ability Completed transfer from C++

--- ai.lisp
+++ ai.lisp
@@ -3,11 +3,13 @@
 
 (defparameter *dictionary* (make-hash-table :test 'equal))
 
+(defparameter *next-neuron-id* 0)
+
 (defstruct neuron
   "An unnamed neuron"
   (threshold 0 :type fixnum)
   (current-value 0 :type fixnum)
-  (id 0 :type fixnum) ; only used for debugging purposes
+  (id (incf *next-neuron-id*) :type fixnum :read-only) ; only used for debugging purposes
   (axon nil))
 
 (defstruct (named-neuron (:include neuron))
@@ -20,6 +22,7 @@
   (weight 0 :type single-float))
 
 (defun create-line ()
+  "Returns a list of named neurons"
   (let (word neuron (res nil))
     (loop
        (setq word (getword))
@@ -52,6 +55,7 @@
        (eol (char s 0))))
 
 (defun getword ()
+  "Get and return a single word from the input stream, return as a string"
 					; if no words, read a non-blank line
   (loop while (>= *current-position* (length *input-line*)) do
        (setq *current-position* 0)
@@ -84,4 +88,54 @@
        (and (and (consp inp)
 		 (not (consp (cdr inp)))
 		 (string= "quit" (named-neuron-name (car inp))))
-	    (return)))))
+	    (return))
+       (build-structure inp)))
+  (dump-dictionary))
+
+(defun connect (prior-neuron next-neuron)
+  "Connect two neurons with a dendrite, return next-neuron"
+  (let ((den (make-dendrite :neuron next-neuron)))
+    (setf (neuron-axon prior-neuron)
+	  (cons den (neuron-axon prior-neuron)))
+    next-neuron))
+
+(defun new-next-neuron (neuron)
+  "Create a new neuron to follow 'neuron' and return it"
+  (connect neuron (make-neuron)))
+
+
+(defun build-structure (inp)
+  "This takes our input list and generates 'every possible combination' into our net"
+  (let (active vn)
+    (dolist (neuron inp)
+	 (let ((next-active (cons (new-next-neuron neuron) nil)))
+	   (setq vn nil) ; it would be better if vn were a fixed size array
+	   ; create extension neurons (ones that represent prior activity) and load up the neuron list (vn)
+	   (dolist (pn active)
+		(setq next-active (cons (new-next-neuron pn) next-active))
+		(setq vn (cons pn vn)))
+
+	   (dolist (pn vn)
+		(let ((nn (new-next-neuron neuron)))
+		  (setq next-active (cons nn next-active))
+		  (connect pn nn)))
+
+	   (setq active next-active)))))
+
+(defun dump-neuron (n level)
+  (dotimes (var level)
+    (princ "    "))
+  (if (named-neuron-p n)
+      (format t "~d (~a)~%"
+	      (neuron-id n)
+	      (named-neuron-name n))
+      (format t "~d~%"
+	     (neuron-id n)))
+  (dolist (den (neuron-axon n))
+    (dump-neuron (dendrite-neuron den) (1+ level))))
+  
+
+(defun dump-dictionary ()
+  (loop for value being the hash-values of *dictionary*
+       do (dump-neuron value 0)))
+
